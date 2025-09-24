608402d2214ecad634fd4634f85f0256477d3ed4
Mateusz Michalik <63094399+Miszalik@users.noreply.github.com>
2025-09-25 00:37:55 +0200

    chore: flask page update

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Witaj w moim środowisku developerskim!</h1><p>Przejdź do /code, /adminer, /portainer, /gitea</p>"
+    return """
+        <h1>Welcome to development environment!</h1>
+        <p>Go to:</p>
+        <ul>
+            <li><a href="/code">code-server</a></li>
+            <li><a href="/adminer">Adminer</a></li>
+            <li><a href="/portainer">Portainer</a></li>
+            <li><a href="/gitea">Gitea</a></li>
+        </ul>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
